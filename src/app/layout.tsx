7edd8119ed0d630d17e83022e4561368da9b32e9
Alex Xu <xulex6@gmail.com>
2024-07-24 13:14:55 +0000

    feat: add trpc and nextauth integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import type { Metadata } from "next";
+import { type Metadata } from "next";
+
+import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,7 +16,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
     </html>
   );
 }
