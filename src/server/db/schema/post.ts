9f93f925de8f73180a9d0ce702ef50afbae39eab
Alex Xu <xulex6@gmail.com>
2024-07-28 12:05:50 +0000

    chore: add multiple folder drizzle orm schema setup

---
+++ src/server/db/schema/post.ts
@@ -0,0 +1,37 @@
+import { sql } from "drizzle-orm";
+import {
+    index,
+    serial,
+    timestamp,
+    varchar,
+} from "drizzle-orm/pg-core";
+import { users } from "../schema";
+import { createTable } from "./util";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+
+export const posts = createTable(
+    "post",
+    {
+        id: serial("id").primaryKey(),
+        name: varchar("name", { length: 256 }),
+        createdById: varchar("created_by", { length: 255 })
+            .notNull()
+            .references(() => users.id),
+        createdAt: timestamp("created_at", { withTimezone: true })
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+            () => new Date()
+        ),
+    },
+    (example) => ({
+        createdByIdIdx: index("created_by_idx").on(example.createdById),
+        nameIndex: index("name_idx").on(example.name),
+    })
+);
