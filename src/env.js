c2fd73668e99d57c02b55e8f6c53b2c89efd2026
Alex Xu <xulexdev@gmail.com>
2024-08-12 05:43:28 +0000

    feat: update env files to support tsx compiling

--- src/env.js
+++ src/env.js
@@ -1,51 +1,140 @@
+// import { createEnv } from "@t3-oss/env-nextjs";
+// import { config } from "dotenv";
+// import { expand } from "dotenv-expand";
+// import { z } from "zod";
+
+// const stringBoolean = z.coerce
+//   .string()
+//   .transform((val) => {
+//     return val === "true";
+//   })
+//   .default("false");
+
+// export const env = createEnv({
+//   /**
+//    * Specify your server-side environment variables schema here. This way you can ensure the app
+//    * isn't built with invalid env vars.
+//    */
+//   server: {
+//     DATABASE_URL: z.string().url(),
+//     NODE_ENV: z
+//       .enum(["development", "test", "production"])
+//       .default("development"),
+//     NEXTAUTH_SECRET:
+//       process.env.NODE_ENV === "production"
+//         ? z.string()
+//         : z.string().optional(),
+//     NEXTAUTH_URL: z.preprocess(
+//       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+//       // Since NextAuth.js automatically uses the VERCEL_URL if present.
+//       (str) => process.env.VERCEL_URL ?? str,
+//       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+//       process.env.VERCEL ? z.string() : z.string().url(),
+//     ),
+//     DISCORD_CLIENT_ID: z.string(),
+//     DISCORD_CLIENT_SECRET: z.string(),
+//     GOOGLE_CLIENT_ID: z.string(),
+//     GOOGLE_CLIENT_SECRET: z.string(),
+//     GITHUB_CLIENT_ID: z.string(),
+//     GITHUB_CLIENT_SECRET: z.string(),
+//     DB_MIGRATING: stringBoolean,
+//     DB_SEEDING: stringBoolean,
+//   },
+
+//   /**
+//    * Specify your client-side environment variables schema here. This way you can ensure the app
+//    * isn't built with invalid env vars. To expose them to the client, prefix them with
+//    * `NEXT_PUBLIC_`.
+//    */
+//   client: {
+//     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+//   },
+
+//   /**
+//    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
+//    * middlewares) or client-side so we need to destruct manually.
+//    */
+//   runtimeEnv: {
+//     DATABASE_URL: process.env.DATABASE_URL,
+//     NODE_ENV: process.env.NODE_ENV,
+//     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+//     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+//     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+//     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+//     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+//     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+//     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+//     DB_MIGRATING: process.env.DB_MIGRATING,
+//     DB_SEEDING: process.env.DB_SEEDING,
+//   },
+//   /**
+//    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+//    * useful for Docker builds.
+//    */
+//   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+//   /**
+//    * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+//    * `SOME_VAR=''` will throw an error.
+//    */
+//   emptyStringAsUndefined: true,
+// });
+
+// expand(config());
+
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z, ZodError } from "zod";
+
+const stringBoolean = z.coerce
+  .string()
+  .transform((val) => {
+    return val === "true";
+  })
+  .default("false");
+
+const EnvSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  DATABASE_URL: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  DB_MIGRATING: stringBoolean,
+  DB_SEEDING: stringBoolean,
+});
+
+expand(config());
+
+try {
+  EnvSchema.parse(process.env);
+} catch (error) {
+  if (error instanceof ZodError) {
+    let message = "Missing required values in .env:\n";
+    error.issues.forEach((issue) => {
+      message += issue.path[0] + "\n";
+    });
+    const e = new Error(message);
+    e.stack = "";
+    throw e;
+  } else {
+    console.error(error);
+  }
+}
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
-  server: {
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
-    ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
-    GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_CLIENT_SECRET: z.string(),
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
-  },
+  server: EnvSchema.shape,
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
-  client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
-  },
+  client: {},
 
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
@@ -54,15 +143,10 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    DB_MIGRATING: process.env.DB_MIGRATING,
+    DB_SEEDING: process.env.DB_SEEDING,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
+
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: true,
 });
