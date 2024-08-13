0ebb82a891874f96820fb9c447a727e0aa2f1095
Alex Xu <xulexdev@gmail.com>
2024-08-13 07:59:22 +0000

    feat: add error code svg graphic components

---
+++ src/components/common/error-404.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+export default function ErrorNotFoundGraphic() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center space-y-4">
+      <p className="max-w-[500px] text-pretty text-center text-3xl font-light sm:text-4xl">
+        oops... page not found
+      </p>
+      <div className="flex flex-col items-center">
+        <Image
+          src="/403-error-not-found.svg"
+          width={500}
+          height={500}
+          alt="404 Error Not Found"
+        />
+        <Button variant="link" asChild>
+          <Link href="https://storyset.com/web">
+            Web illustrations by Storyset
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/protect.tsx
@@ -0,0 +1,46 @@
+import React, { type ReactNode } from "react";
+
+import { checkCoursePermission, getServerAuthSession } from "@/server/auth";
+
+import ErrorForbiddenGraphic from "@/components/common/error-403";
+
+interface ProtectProps {
+  courseId: string;
+  permissionSlug: string;
+  isMember?: boolean;
+  hidden?: boolean;
+  children: ReactNode;
+}
+
+export default async function Protect({
+  courseId,
+  permissionSlug,
+  isMember = false,
+  hidden = false,
+  children,
+}: ProtectProps) {
+  const session = await getServerAuthSession();
+
+  if (!session || !session.user || !session.user.id) {
+    return hidden ? null : <ErrorForbiddenGraphic />; // or return a fallback component like <Unauthorized />
+  }
+
+  const userId = session.user.id;
+
+  const hasPermission = await checkCoursePermission(
+    userId,
+    courseId,
+    permissionSlug,
+  );
+
+  // !session.user.isSuperuser && !hasPermission && isMember -> pass
+  // !session.user.isSuperuser && !hasPermission && !isMember -> fail
+  // !session.user.isSuperuser && hasPermission && !isMember -> pass
+  // session.user.isSuperuser && !hasPermission && !isMember -> pass
+
+  if (!session.user.isSuperuser && !hasPermission && !isMember) {
+    return hidden ? null : <ErrorForbiddenGraphic />;
+  }
+
+  return <>{children}</>;
+}
