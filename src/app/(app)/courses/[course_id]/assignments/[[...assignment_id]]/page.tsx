1ed852608b1ae093d368dc94b2ac654ceecc06c3
Alex Xu <xulexdev@gmail.com>
2024-09-14 10:57:22 +0000

    feat: add assignment edit form to right side of layout;

--- src/app/(app)/courses/[course_id]/assignments/[[...assignment_id]]/page.tsx
+++ src/app/(app)/courses/[course_id]/assignments/[[...assignment_id]]/page.tsx
@@ -2,8 +2,10 @@ import { notFound } from "next/navigation";
 
 import { getAssignmentById } from "@/server/api/crud/assignments/queries";
 import { getDeliverablesByAssignment } from "@/server/api/crud/deliverables/queries";
+import { verifyProtectedPermission } from "@/server/auth";
 import { ClipboardList } from "lucide-react";
 
+import { PERM_ASSIGNMENT_MANAGE_CORE } from "@/lib/constants";
 import { Textarea } from "@/components/ui/textarea";
 import AssignmentDeleteForm from "@/components/forms/assignments/assignment-delete-form";
 import AssignmentUpdateForm from "@/components/forms/assignments/assignment-update-form";
@@ -26,9 +28,31 @@ export default async function AssignmentPage({ params }: AssignmentPageProps) {
 
   const { deliverables } = await getDeliverablesByAssignment(assignmentId);
 
+  // if assignment is inactive and user doesn't have permission to view inactive assignments, use the Protect component
+
+  const isActive =
+    assignment.availableAt && assignment.availableAt < new Date();
+
+  const { access: hasViewInactiveAssingmentPermission } =
+    await verifyProtectedPermission(
+      params.course_id,
+      PERM_ASSIGNMENT_MANAGE_CORE,
+    );
+  const { access: hasAssignmentEditPermission } =
+    await verifyProtectedPermission(
+      params.course_id,
+      PERM_ASSIGNMENT_MANAGE_CORE,
+    );
+  if (!isActive && !hasViewInactiveAssingmentPermission) {
+    return <AssignmentsEmptyState />;
+  }
+
   return (
     <div className="space-y-8">
-      <AssignmentUpdateForm assignment={assignment} editing={true} />
+      <AssignmentUpdateForm
+        assignment={assignment}
+        editing={hasAssignmentEditPermission}
+      />
 
       <div>
         <h4 className="mb-2 text-lg font-medium">Outline</h4>
@@ -36,27 +60,34 @@ export default async function AssignmentPage({ params }: AssignmentPageProps) {
           className="w-full"
           rows={25}
           defaultValue="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repellat ipsum laborum at iure? Veniam consequuntur aut voluptatibus esse dolor quae quos, dolore, voluptatem repellat, incidunt aspernatur nulla placeat facere."
+          disabled={!hasAssignmentEditPermission}
         />
       </div>
       <div>
-        <div className="mb-2 flex items-end justify-between">
-          <h4 className="text-lg font-medium">Deliverables</h4>
-          <DeliverableCreationDialog
-            courseId={params.course_id}
-            assignmentId={assignment.id}
-          />
-        </div>
+        <h4 className="mb-2 text-lg font-medium">Deliverables</h4>
+        {/* <div className="mb-2 flex items-end justify-between">
+        </div> */}
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-3">
-          {deliverables.map((deliverable) => (
-            <DeliverableCard
-              status={""}
-              key={deliverable.id}
-              {...deliverable}
-            />
-          ))}
+          <>
+            {deliverables.map((deliverable) => (
+              <DeliverableCard
+                status={""}
+                key={deliverable.id}
+                {...deliverable}
+              />
+            ))}
+            {hasAssignmentEditPermission && (
+              <DeliverableCreationDialog
+                courseId={params.course_id}
+                assignmentId={assignment.id}
+              />
+            )}
+          </>
         </div>
       </div>
-      <AssignmentDeleteForm assignment={assignment} />
+      {hasAssignmentEditPermission && (
+        <AssignmentDeleteForm assignment={assignment} />
+      )}
     </div>
   );
 }
