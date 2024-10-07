dc1e91c2268dbdd4e0e39470b7a8462be7c06a86
Alex Xu <xulexdev@gmail.com>
2024-10-07 03:41:58 +0000

    feat: add boolean fields for group assignments, self enrolment, conform tutorial grouping and a max group size field for assignments

--- src/server/db/schema/assignment.ts
+++ src/server/db/schema/assignment.ts
@@ -1,5 +1,11 @@
 import { sql } from "drizzle-orm";
-import { integer, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
@@ -31,6 +37,11 @@ export const assignments = createTable("assignment", {
     withTimezone: true,
   }),
   weighting: integer("weighting").notNull().default(0),
+  isGroup: boolean("is_group").notNull().default(false),
+  isSelfEnrol: boolean("is_self_enrol").notNull().default(false),
+  isTutorialGrouping: boolean("is_tutorial_grouping").notNull().default(false),
+  maxGroupSize: integer("max_group_size").notNull().default(1),
+  isArchived: boolean("is_archived").notNull().default(false),
   createdAt: timestamp("created_at", {
     mode: "date",
     withTimezone: true,
@@ -52,7 +63,7 @@ export const insertAssignmentParams = baseAssignmentSchema.pick({
 export const updateAssignmentSchema = baseAssignmentSchema;
 export const updateAssignmentParams = baseAssignmentSchema.extend({
   name: z.string().min(1, "Assignment name is required."),
-  weighting: z
+  weighting: z.coerce
     .number()
     .int()
     .refine((val: number) => {
@@ -60,6 +71,9 @@ export const updateAssignmentParams = baseAssignmentSchema.extend({
     }, "Weighting must be an integer between 0 and 100."),
   availableAt: z.date().nullable(),
   editorId: z.string().nullable(),
+  maxGroupSize: z.coerce
+    .number({ invalid_type_error: "Max team size must be a number" })
+    .positive("Team Size must be positive"),
 });
 
 export const deleteAssignmentSchema = baseAssignmentSchema.pick({
